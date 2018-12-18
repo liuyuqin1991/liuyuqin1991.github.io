2247f0e16483573300a9c47ba4c8fca74c84499f
liuyuqin <liuyuqin@jd.com>
2018-12-18 11:11:31 +0800

    Updates

---
+++ precache-manifest.78977c727e0735fec740036044f6e403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bffa24b66a876a08a38",
+    "url": "/static/js/main.9bffa24b.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "9bffa24b66a876a08a38",
+    "url": "/static/css/main.9f84aed8.chunk.css"
+  },
+  {
+    "revision": "5903b727a609703c6b047ddaca3e1a22",
+    "url": "/index.html"
+  }
+];
