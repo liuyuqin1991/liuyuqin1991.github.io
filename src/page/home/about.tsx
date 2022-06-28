385b76ca3b702d44d9de72e500f38528740fc67d
slashliu@yunify.com <slashliu@yunify.com>
2022-06-28 11:48:19 +0800

    update: 更新技能，关于，项目经验

--- src/page/home/about.tsx
+++ src/page/home/about.tsx
@@ -7,39 +7,44 @@ import JuejinIcon from '../../assets/image/home/juejin.svg';
 import BilibiliIcon from '../../assets/image/home/bilibili.svg';
 
 class AboutComponent extends BaseComponent {
+  renderWebIcons() {
+    const svgIcon: { [key: string]: string } = {
+      github: GithubIcon,
+      csdn: CsdnIcon,
+      juejin: JuejinIcon,
+      bilibili: BilibiliIcon,
+    };
+    const webs = AboutJson.webs;
+    return webs.map(function (item) {
+      return (
+        <>
+          <a href={item.web}>
+            <img src={svgIcon[item.name]} alt={item.name}></img>
+          </a>
+        </>
+      );
+    });
+  }
 
-    renderWebIcons() {
-        const svgIcon: { [key: string]: string } = {
-            github: GithubIcon, csdn: CsdnIcon,juejin: JuejinIcon, bilibili: BilibiliIcon
-        };
-        const webs = AboutJson.webs;
-        return webs.map(function(item) {
-            return (
-                <>
-                    <a href={item.web}>
-                        <img src={svgIcon[item.name]} alt={item.name}></img>
-                    </a>
-                </>
-            );
-        });
-    }
-
-    render() {
-        return <div className="module-view about">
-            <div className="module-title h1 user">About Me</div>
-            <div className="module-title h2">信息</div>
-            <div className="about-info name">姓名：{AboutJson.name}</div>
-            <div className="about-info">年龄：{AboutJson.age}</div>
-            <div className="about-info">坐标：{AboutJson.address}</div>
-            <div className="about-info">工龄：{AboutJson.experience}</div>
-            <div className="about-info">电话：{AboutJson.phone}</div>
-            <div className="about-info">邮箱：{AboutJson.email}</div>
-            <div className="module-title h2">个人介绍</div>
-            <div className="about-introduction">{AboutJson.introduction}</div>
-            <div className="module-title h2">个人网站</div>
-            <div className="about-web-icon">{this.renderWebIcons()}</div>
-        </div>
-    }
+  render() {
+    return (
+      <div className="module-view about">
+        <div className="module-title h1 user">About Me</div>
+        <div className="module-title h2">信息</div>
+        <div className="about-info name">姓名：{AboutJson.name}</div>
+        <div className="about-info">年龄：{AboutJson.age}</div>
+        <div className="about-info">坐标：{AboutJson.address}</div>
+        <div className="about-info">工龄：{AboutJson.experience}</div>
+        <div className="about-info">电话：{AboutJson.phone}</div>
+        <div className="about-info">邮箱：{AboutJson.email}</div>
+        <div className="about-info">求职：{AboutJson.job}</div>
+        <div className="module-title h2">个人介绍</div>
+        <div className="about-introduction">{AboutJson.introduction}</div>
+        <div className="module-title h2">个人网站</div>
+        <div className="about-web-icon">{this.renderWebIcons()}</div>
+      </div>
+    );
+  }
 }
 
 export default AboutComponent;
